fe13c6c7c0027e3810649866d91d1a7043627aef
adrialcaide <adalji96@gmail.com>
2022-12-06 10:27:53 +0100

    day06 puzzles

---
+++ day06/comms2.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+var input = fs.readFileSync('commsinput.txt', 'utf-8');
+
+var window = []
+for (var i = 0; i < input.length; i++) {
+    window.push(input[i]);
+
+    if (window.length == 14) {
+        if (allDifferent(window)) {
+            console.log(i + 1);
+            break;
+        }
+
+        window.shift();
+    }
+}
+
+
+function allDifferent(arr) {
+    return arr.length == [...new Set(arr)].length;
+}
+
